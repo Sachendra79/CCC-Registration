bd98ddf681e629f6b1a704c8e41fd6455125da4c
Yash Mishra <praisedboy5000@gmail.com>
2023-12-09 00:06:52 +0530

    corrected some amount of media query

--- src/Pages/Faculty.js
+++ src/Pages/Faculty.js
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from "react";
 import Card from "../Components/Card";
 import card from "../assets/Card.svg";
 import middle from "../assets/Image_middle.svg";
 
 const Faculty = () => {
-  
   return (
     <div>
-       <div className='Header'></div>
-       <div className='faculty-content'>
-
-       <Card />
+      <div className="Header"></div>
+      <div className="faculty-content">
+        <Card />
         <h1>Faculty Coordinators</h1>
         <div className="WhiteCard">
-          <img src={card} alt="ImageNA" className='Image' />
-          <img src={middle} alt="ImageNA" />
-          <img src={card} alt="ImageNA" className='Image' />
+            <img src={card} alt="ImageNA" className="Image" />
+            <img src={middle} alt="ImageNA" className="design"/>
+            <img src={card} alt="ImageNA" className="Image" />
         </div>
-       </div>
-       <div className='footer'></div>
+      </div>
+      <div className="footer"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Faculty
+export default Faculty;
