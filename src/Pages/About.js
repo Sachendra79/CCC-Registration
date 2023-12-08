8c4e9070fe892e9ba9c5baae70a22b61e9bd61e6
Yash Mishra <praisedboy5000@gmail.com>
2023-12-08 23:39:04 +0530

    added some amount of media queries in 2 pages

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,19 @@
 import React from "react";
-
+import Card from '../Components/Card'
+import Design from '../assets/Design.svg'
 
 const About = () => {
-  
+  const Image_design = {
+    backgroungImage: 'none',
+  }
   return (
     <div>
       <div className="about-content">
+        <Card />
+        <h1>### Mastering Algorithms: Mr. Rohit Negi</h1>
+        <div className="WhiteCard">
+          <img src={Design} alt="ImageNA" style={Image_design} />
+        </div>
       </div>
       <div className="footer"></div>
     </div>
